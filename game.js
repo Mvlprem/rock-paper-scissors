7c48be1e51175488ac0171bbf54342f7221a825c
Mvlprem <mvl.prem@outlook.com>
2023-04-12 07:07:17 +0530

    refactor: code clean up

--- game.js
+++ game.js
@@ -42,6 +42,106 @@ function updateScore(computer, player){
 }
 
 /*
+variables to store win count
+*/
+let computerScore = 0;
+let playerScore = 0;
+
+/*
+This function takes in both player choices and
+checks for possible winner and updates their win count.
+
+@param computerChoice
+@param playerChoice
+*/
+function playRound(computerSelection, playerSelection){
+
+    let roundResult = undefined;
+    let resultInfo = undefined;
+    
+    switch(computerSelection){
+        case `rock`:
+            if (computerSelection ===`rock` && playerSelection === `paper`){
+                roundResult = `You won!`;
+                resultInfo = `Paper beats rock`;
+                playerScore++;
+                
+            }else if (computerSelection ===`rock` && playerSelection === `scissor`){
+                roundResult = `You lost!`;
+                resultInfo = `Scissor is beaten by rock`;
+                computerScore++;
+                
+            }else { 
+                roundResult = `It's a tie!`;
+                resultInfo = `Rock ties with rock`;
+            }
+            break;
+
+        case `paper`:
+            if (computerSelection ===`paper` && playerSelection === `rock`){
+                roundResult = `You lost!`;
+                resultInfo = `Rock is beaten by paper`;
+                computerScore++;
+                
+            }else if (computerSelection ===`paper` && playerSelection === `scissor`){
+                roundResult = `You won!`;
+                resultInfo = `Scissor beats paper`;
+                playerScore++;
+
+            }else { 
+                roundResult = `It's a tie!`;
+                resultInfo = `Paper ties with paper`;
+            }
+            break;
+
+        case `scissor`:
+            if (computerSelection ===`scissor` && playerSelection === `paper`){
+                roundResult = `You lost!`;
+                resultInfo = `Paper is beaten by scissor`;
+                computerScore++;
+                
+            }else if (computerSelection ===`scissor` && playerSelection === `rock`){
+                roundResult = `You won!`;
+                resultInfo = `Rock beats scissor`;
+                playerScore++;
+                
+            }else { 
+                roundResult = `It's a tie!`;
+                resultInfo = `Scissor ties with scissor`; 
+            }
+            break;
+            
+            default: console.log(`Something went wrong, try contacting admin.`);
+    }
+        
+    displayRoundInfo(roundResult, resultInfo);
+    updateScore(computerScore, playerScore);
+
+}
+    
+/*
+whoever scores the first five points wins
+and the game ends announcing the result
+*/
+function endGame(){
+
+    if (computerScore === 5){
+        
+        setTimeout(function() {
+            alert(`You Loose!`);
+        },10)
+        location.reload();
+        
+    }else if (playerScore === 5){
+
+        setTimeout(function() {
+            alert(`You Won!`);
+        },10)
+        location.reload();
+    }
+}
+
+/*
 on click gets both players choices and
 displays selected hand signs by calling 'displaySelectedSigns()'
 checks for round winner by calling 'playRound()'
@@ -60,103 +160,3 @@ button.forEach(button => button.addEventListener(`click`, (e) => {
     playRound(computerChoice, playerChoice);
     endGame();
 }));
-    
-/*
-variables to store win count
-*/
-let computerScore = 0;
-let playerScore = 0;
-
-/*
-This function takes in both player choices and
-checks for possible winner and updates their win count.
-
-@param computerChoice
-@param playerChoice
-*/
-function playRound(computerSelection, playerSelection){
-
-    let roundResult = undefined;
-    let resultInfo = undefined;
-
-    switch(computerSelection){
-        case `rock`:
-            if (computerSelection ===`rock` && playerSelection === `paper`){
-                roundResult = `You won!`;
-                resultInfo = `Paper beats rock`;
-                playerScore++;
-
-            }else if (computerSelection ===`rock` && playerSelection === `scissor`){
-                roundResult = `You lost!`;
-                resultInfo = `Scissor is beaten by rock`;
-                computerScore++;
-
-            }else { 
-                roundResult = `It's a tie!`;
-                resultInfo = `Rock ties with rock`;
-            }
-            break;
-
-        case `paper`:
-            if (computerSelection ===`paper` && playerSelection === `rock`){
-                roundResult = `You lost!`;
-                resultInfo = `Rock is beaten by paper`;
-                computerScore++;
-
-            }else if (computerSelection ===`paper` && playerSelection === `scissor`){
-                roundResult = `You won!`;
-                resultInfo = `Scissor beats paper`;
-                playerScore++;
-
-            }else { 
-                roundResult = `It's a tie!`;
-                resultInfo = `Paper ties with paper`;
-            }
-            break;
-
-        case `scissor`:
-            if (computerSelection ===`scissor` && playerSelection === `paper`){
-                roundResult = `You lost!`;
-                resultInfo = `Paper is beaten by scissor`;
-                computerScore++;
-
-            }else if (computerSelection ===`scissor` && playerSelection === `rock`){
-                roundResult = `You won!`;
-                resultInfo = `Rock beats scissor`;
-                playerScore++;
-
-            }else { 
-                roundResult = `It's a tie!`;
-                resultInfo = `Scissor ties with scissor`; 
-            }
-            break;
-
-        default: console.log(`Something went wrong, try contacting admin.`);
-    }
-
-    displayRoundInfo(roundResult, resultInfo);
-    updateScore(computerScore, playerScore);
-}
-
-/*
-whoever scores the first five points wins
-and the game ends announcing the result
-*/
-function endGame(){
-
-    if (computerScore === 5){
-
-        setTimeout(function() {
-            alert(`You Loose!`);
-        },10)
-        location.reload();
-
-    }else if (playerScore === 5){
-
-        setTimeout(function() {
-            alert(`You Won!`);
-        },10)
-        location.reload();
-    }
-}
-
